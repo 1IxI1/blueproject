f866aa5283b9f6f650d648d74bc243919d7767be
Victor Tec <syshchenko228@mail.ru>
2023-12-28 23:56:36 +0300

    working vite

---
+++ dapp/src/wrappers/VoteKeeper.ts
@@ -0,0 +1,40 @@
+import {
+  Address,
+  beginCell,
+  Cell,
+  Contract,
+  contractAddress,
+  ContractProvider,
+  Sender,
+  SendMode,
+  toNano,
+} from "@ton/core";
+
+export class VoteKeeper implements Contract {
+  constructor(
+    readonly address: Address,
+    readonly init?: { code: Cell; data: Cell },
+  ) {}
+
+  static createFromAddress(address: Address) {
+    return new VoteKeeper(address);
+  }
+  /*
+(voter_wallet, voting, votes)
+*/
+  async getData(provider: ContractProvider) {
+    let res = await provider.get("get_vote_keeper_data", []);
+    let voter_wallet = res.stack.readAddress();
+    let voting = res.stack.readAddress();
+    let votesFor = res.stack.readBigNumber();
+    let votesAgainst = res.stack.readBigNumber();
+    let totalVotes = votesFor + votesAgainst;
+    return {
+      voter_wallet,
+      voting,
+      totalVotes,
+      votesFor,
+      votesAgainst,
+    };
+  }
+}
